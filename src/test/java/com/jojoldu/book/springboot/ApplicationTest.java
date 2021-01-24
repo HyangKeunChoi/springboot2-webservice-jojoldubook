296a88bba62d2043cb02bb43445144b6fa3aaed8
hyangkeunchoi <gidrms99@naver.com>
2021-01-24 23:01:49 +0900

    p180까지

---
+++ src/test/java/com/jojoldu/book/springboot/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot;
+
+import org.junit.After;
+
+import static org.junit.Assert.*;
+public class ApplicationTest {
+
+    @After
+    public void tearDown() {
+    }
+}
